ab72c1b78cd3805b01be70442fc74ecb182e1d0b
Aaron Meihm <ameihm@mozilla.com>
2015-05-21 13:29:46 -0500

    add some comments regarding mig callout

--- src/wfs/mig.go
+++ src/wfs/mig.go
@@ -16,21 +16,32 @@ import (
 	"strings"
 )
 
+// XXX This function expects the GPG passphrase used by mig to have already
+// been cached by the GPG agent. If it's not, pinentry will be called in this
+// function and depending on the system the input dialog may not show up.
+//
+// When this happens the process will be waiting for the call into GPG
+// agent to return. We need a workaround to detect this scenario, or a way
+// to tell MIG to immediately return if the key needs to be decrypted.
 func migGetCandidates(desc descriptorMig) (cand []fileCandidate, err error) {
 	fmt.Fprintf(os.Stdout, "[descriptor] executing mig query\n")
+
 	var pfre *regexp.Regexp
 	if desc.PostFilter != "" {
 		pfre = regexp.MustCompile(desc.PostFilter)
 	}
+
 	migargs, err := desc.buildMigArguments()
 	if err != nil {
 		return cand, err
 	}
+
 	cmd := exec.Command(config.Main.MIG, migargs...)
 	out, err := cmd.Output()
 	if err != nil {
 		return cand, err
 	}
+
 	rdr := strings.NewReader(string(out))
 	scanner := bufio.NewScanner(rdr)
 	for scanner.Scan() {
@@ -42,6 +53,9 @@ func migGetCandidates(desc descriptorMig) (cand []fileCandidate, err error) {
 		if len(elem) < 2 {
 			return cand, fmt.Errorf("malformed output from mig: %v", buf)
 		}
+		// XXX Probably want to add some additional validation of the
+		// data that is being returned by mig, to make sure it is a
+		// valid hostname and file path.
 		if pfre != nil {
 			if !pfre.MatchString(elem[1]) {
 				continue
